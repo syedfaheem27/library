da302714bf75044425b8c9d4fed4482e86dba9a0
syedfaheem27 <syedfaheem2719@gmail.com>
2023-03-17 18:29:49 +0530

    Add a function formAppear which displays a form on the page asking for the information about a book and it gets invoked on clicking the Add Book button.

--- script.js
+++ script.js
@@ -1,48 +1,97 @@
 let myLibrary=[];
 
 
-function Book(title,author,pages,status) {
-  this.Title=title;
-  this.Author=author;
-  this.Pages=pages;
-  this.Status=status;
+function Book(array) {
+  this.Title=array[0].value;
+  this.Author=array[1].value;
+  this.Pages=array[2].value;
+  this.Status=array[3].checked;
 }
 
 
 
-function addBookToLibrary() {
-  const div_body=document.querySelector('.body');
-  for(let i=0;i<myLibrary.length;i++) {
-    const div_container=document.createElement('div');
-    div_body.appendChild(div_container);
+function addBookToLibrary(form) {
+  const formNodes=Array.from(form.childNodes);
+  console.log(formNodes);
+  const inputArray=formNodes.filter((input)=> {
+    return (input.getAttribute('data-name')=='input');
+  })
+  console.log(inputArray);
+  console.log(inputArray[3].checked);
+  myLibrary[n]=new Book(inputArray);
+  console.log(myLibrary[n])
 
-    for(let prop in myLibrary[i]){
-      if(prop=='Status'){
-      const div=document.createElement('div');
-      div_container.appendChild(div);
-      if(myLibrary[i][prop]){
-        div.textContent=prop+': Read';
-      }
-      else{
-        div.textContent=prop+': Not read';
-      }
-      }
-      else{
-      const div=document.createElement('div');
-      div_container.appendChild(div);
-      div.textContent=prop+': '+myLibrary[i][prop];
-      }
+
+}
+
+
+const addbook=document.querySelector('#add');
+addbook.addEventListener('click',formAppear);
+
+let submit_flag=true;
+//ensures that a new form is loaded once the user submits the existing form
+let n=0;
+
+function formAppear() {
+  if(submit_flag){
+  const form=document.createElement('form');
+  document.querySelector('body')
+  .appendChild(form);
+
+  const myLabels=[],
+  myInputs=[];
+
+  for(let i=0;i<4;i++){
+    myLabels[i]=document.createElement('label');
+    myInputs[i]=document.createElement('input');
+    form.appendChild(myLabels[i]);
+    form.appendChild(myInputs[i]);
+
+    switch(i) {
+      case 0:
+        myLabels[i].setAttribute('for','title');
+        myLabels[i].textContent='Title';
+        myInputs[i].setAttribute('type','text');
+        myInputs[i].setAttribute('id','title');
+        myInputs[i].setAttribute('data-name','input');
+        break;
+
+        case 1:
+          myLabels[i].setAttribute('for','author');
+          myLabels[i].textContent='Author';
+          myInputs[i].setAttribute('type','text');
+          myInputs[i].setAttribute('id','author');
+          myInputs[i].setAttribute('data-name','input');
+          break;
+
+        case 2:
+          myLabels[i].setAttribute('for','pages');
+          myLabels[i].textContent='Pages';
+          myInputs[i].setAttribute('type','number');
+          myInputs[i].setAttribute('id','pages');
+          myInputs[i].setAttribute('data-name','input');
+          break;
+
+        default:
+          myLabels[i].setAttribute('for','status');
+          myLabels[i].textContent='Read';
+          myInputs[i].setAttribute('type','checkbox');
+          myInputs[i].setAttribute('id','status');
+          myInputs[i].setAttribute('data-name','input');
+          break;
     }
-  
+  }
+  const submit_button=document.createElement('button');
+  form.appendChild(submit_button);
+  submit_button.setAttribute('type','button');
+  submit_button.textContent="Submit";
 
-    
+  submit_button.addEventListener('click',()=>{
+    addBookToLibrary(form);
+  });
+  }
+  else {
+    return;
+  }
 
 }
-}
-
-
-//demo books
-myLibrary[0]=new Book('Alchemist','Paulo coelho',295,true);
-myLibrary[1]=new Book('Kite Runner','Khalid Hosseini',300,false);
-
-addBookToLibrary();
