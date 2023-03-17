9a60c93c1e61b8d927039044e8c2ebf514aa965d
syedfaheem27 <syedfaheem2719@gmail.com>
2023-03-17 15:05:39 +0530

    Add two books in myLibrary array and a function that loops through the objects and puts them onto the web page and also add their styling.

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ function Book(title,author,pages,status) {
 
 
 function addBookToLibrary() {
+  const div_body=document.querySelector('.body');
+  for(let i=0;i<myLibrary.length;i++) {
+    const div_container=document.createElement('div');
+    div_body.appendChild(div_container);
 
+    for(let prop in myLibrary[i]){
+      const div=document.createElement('div');
+      div_container.appendChild(div);
+      div.textContent=`${prop}: ${myLibrary[i][prop]}`
+    }
+  
+
+    
 
 }
+}
 
+
+//demo books
+myLibrary[0]=new Book('Alchemist','Paulo coelho',295,true);
+myLibrary[1]=new Book('Kite Runner','Khalid Hosseini',300,false);
+
+addBookToLibrary();
